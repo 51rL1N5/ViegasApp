677da3ceebda761aaddb86680568e756b1776b6d
51rL1N5 <lipeols@hotmail.com>
2018-10-24 22:47:55 -0300

    Feita poucas mudanças na classe servidor, definição de padrões

--- servidor.py3.py
+++ servidor.py3.py
@@ -1,27 +1,75 @@
 # UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE
 # DEPARTAMENTO DE ENGENHARIA DE COMPUTACAO E AUTOMACAO
 # DISCIPLINA REDES DE COMPUTADORES (DCA0113)
-# AUTOR: PROF. CARLOS M D VIEGAS (viegas 'at' dca.ufrn.br)
+# AUTOR: ...
 #
-# SCRIPT: Servidor de sockets TCP modificado para receber texto minusculo do cliente enviar resposta em maiuscula
-#
+# SCRIPT: ...
 
 # importacao das bibliotecas
 from socket import *                       # sockets
+from threading import Thread
 
-# definicao das variaveis
-serverName = ''                            # ip do servidor (em branco)
-serverPort = 12000                         # porta a se conectar
-serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
-serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
-serverSocket.listen(1)                     # socket pronto para 'ouvir' conexoes
-print ('Servidor TCP esperando conexoes na porta %d ...' % (serverPort))
-while 1:
-  connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
-  sentence = connectionSocket.recv(1024)         # recebe dados do cliente
-  sentence = sentence.decode('utf-8')
-  capitalizedSentence = sentence.upper()         # converte em letras maiusculas
-  print ('Cliente %s enviou: %s, transformando em: %s' % (addr, sentence, capitalizedSentence))
-  connectionSocket.send(capitalizedSentence.encode('utf-8')) # envia para o cliente o texto transformado
-  connectionSocket.close() # encerra o socket com o cliente
-serverSocket.close() # encerra o socket do servidor
+
+class Client(threading.Thread):
+    def __init__(nickName, ip, port):
+        self.nickName = nickName
+        self.ip = ip
+        self.port = port
+        # self.socket = socket(AF_INET, SOCK_STREAM)
+        # self.socket.bind(PORT)
+        # flag para comunicacao privada ?
+        # nick do usuario com quem este esta em privado ?
+        pass
+    def run(): #metodo para a thread
+        pass
+
+
+
+class Server(threading.Thread):
+    def __init__(port):
+        # configura o servidor
+        self.serverName = ''                            # ip do servidor (em branco)
+        self.serverPort = port                         # porta a se conectar
+        self.serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
+        self.serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
+        # inicia o servidor
+        self.clients = []#vetor de client
+    def run():
+        # função para chamar a thread do bate papo
+        # ASDFADFADFAFDASFADSFASD()
+
+        self.serverSocket.listen(1)                     # socket pronto para 'ouvir' conexoes
+
+        print ('Servidor TCP esperando conexoes na porta %d ...' % (serverPort))
+
+        # -------------------
+        self.manager(serverSocket);
+        # --- manager == main
+
+    def manager(serverSocket):
+        # Gerencia os usuários
+
+        while True:
+          connectionSocket, addr = self.serverSocket.accept() # aceita as conexoes dos clientes
+
+          nickname = connectionSocket.recv(1024)
+
+          if len(nickname) == 0:
+              connectionSocket.close()
+              continue
+
+          self.clients.append([nickname, addr, connectionSocket])
+
+        serverSocket.close() # encerra o socket do servido
+
+
+
+        self.clients.append( Client(Nick, IP, port) );
+
+        """
+        A thread principal irá recepcionar o usuário novo
+        Perguntará o seu nick.
+
+        Em sequência, ela enviara uma Tupla contendo o ip do usuário + nickname
+        """
+        pass
