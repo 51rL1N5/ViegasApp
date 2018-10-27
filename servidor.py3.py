c86ef7910832f225a37b3b719d2596206677b0f2
51rL1N5 <lipeols@hotmail.com>
2018-10-26 22:17:55 -0300

    É a nova era

--- servidor.py3.py
+++ servidor.py3.py
@@ -11,10 +11,10 @@ from threading import Thread
 
 
 class Client(threading.Thread):
-    def __init__(nickName, ip, port):
+    def __init__(nickName, ip, socket):
         self.nickName = nickName
         self.ip = ip
-        self.port = port
+        self.socket = socket
         # self.socket = socket(AF_INET, SOCK_STREAM)
         # self.socket.bind(PORT)
         # flag para comunicacao privada ?
@@ -58,13 +58,15 @@ class Server(threading.Thread):
               connectionSocket.close()
               continue
 
-          self.clients.append([nickname, addr, connectionSocket])
+          cliente = Client(nickname, addr, connectionSocket)
+          self.clients.append(cliente)
+
+          welcome = nicname + " acabou de entrar"
+
+          send_for_all(welcome)
 
         serverSocket.close() # encerra o socket do servido
 
-
-
-        self.clients.append( Client(Nick, IP, port) );
 
         """
         A thread principal irá recepcionar o usuário novo
@@ -72,4 +74,51 @@ class Server(threading.Thread):
 
         Em sequência, ela enviara uma Tupla contendo o ip do usuário + nickname
         """
+        pass
+
+    def send_for_all(message):
+        # ENVIAR PRA GALERA KKKKKKK
+        try:
+            for user in self.clients:
+                user.socket.send(message)
+        except Error:
+            print ("ERROR ON CHAT -> %s" % Error)
+        pass
+
+    def chat():
+        """
+        Função do chat (SALVE JOAO DÓRIA Kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk???????)
+        """
+
+        print("Chat iniciado com sucesso")
+
+        while 1:
+            # RECEBER MENSAGEM
+            message = self.socket.recv(1024)
+            prompt = message.slice("(")
+            directive = prompt[1].slice(")")
+
+            # Tratar mensagem, para caso ser um comando
+
+            if   prompt[1] == "lista":
+                for user in self.clients:
+                    send_for_all("<" + str(user.nick) +", " + str(user.IP) + ", " + str(user.socket))
+            elif prompt[1] == "privado":
+
+            elif prompt[1] == "nome":
+                """BOTAR AQUI O NICK DO CLIENTE AGORA É NOVO-JOAO-DORIA"""
+                send_for_all()
+            elif prompt[1] == "sair":
+
+                """ BOTAR AQUI QUE O CARA ACABOU DE SAIR, TEM QUE PEGAR O NOME DELE"""
+                send_for_all()
+            else:
+                send_for_all(message)
+
+
+
+
+
+
+
         pass
