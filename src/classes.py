0a9e504c8a3738621bd15d5c207e07b3073d429b
51rL1N5 <lipeols@hotmail.com>
2018-10-27 22:28:49 -0300

    Organizando melhor

--- src/classes.py
+++ src/classes.py
@@ -1,3 +1,19 @@
 # Classe
 # Frame
 # ...
+
+class Frame:
+    def __init__(self, ip_dest, ip_orig, nickname, command, data):
+        self.length   = len(data)
+        self.ip_orig  = ip_orig
+        self.ip_dest  = ip_dest
+        self.nickname = nickname
+        self.command  = command
+        self.data     = data
+        pass
+
+class User:
+    def __init__(self, nickname, addr):
+        self.nickname = nickname
+        self.addr     = addr
+        pass
